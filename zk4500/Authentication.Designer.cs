6c0cb14febd6445ba7839467424e779fa0c6c14c
3bmo3ty <mohamedabdalmoty3@gmail.com>
2022-06-29 02:44:11 +0200

    initial  commit

---
+++ zk4500/Authentication.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace zk4500
+{
+    partial class Authentication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fpicture = new System.Windows.Forms.PictureBox();
+            this.prompt = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Put your index finger on the scanner";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::zk4500.Properties.Resources.WhatsApp_Image_2021_12_13_at_6_14_27_PM;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(730, 492);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // fpicture
+            // 
+            this.fpicture.BackColor = System.Drawing.SystemColors.Window;
+            this.fpicture.Location = new System.Drawing.Point(914, 165);
+            this.fpicture.Name = "fpicture";
+            this.fpicture.Size = new System.Drawing.Size(268, 283);
+            this.fpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fpicture.TabIndex = 36;
+            this.fpicture.TabStop = false;
+            // 
+            // prompt
+            // 
+            this.prompt.AutoSize = true;
+            this.prompt.Location = new System.Drawing.Point(911, 451);
+            this.prompt.Name = "prompt";
+            this.prompt.Size = new System.Drawing.Size(50, 13);
+            this.prompt.TabIndex = 38;
+            this.prompt.Text = "Ready ...";
+            // 
+            // Authentication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 701);
+            this.Controls.Add(this.prompt);
+            this.Controls.Add(this.fpicture);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Authentication";
+            this.Text = "Authentication";
+            this.Load += new System.EventHandler(this.Authentication_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox fpicture;
+        private System.Windows.Forms.Label prompt;
+    }
+}
---
+++ zk4500/Authentication.cs
@@ -0,0 +1,212 @@
+﻿using AxZKFPEngXControl;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+namespace zk4500
+{
+    public partial class Authentication : Form
+    {
+        private AxZKFPEngX ZkFprint = new AxZKFPEngX();
+        private bool Check;
+        String userTemplate;
+        public Authentication()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Authentication_Load(object sender, EventArgs e)
+        {
+            Controls.Add(ZkFprint);
+            InitialAxZkfp();
+            Play();
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            //webPostMethod("000", "https://moproject.herokuapp.com/matching/");
+            String temp = webGetMethod("https://moproject.herokuapp.com/templates");
+
+            if (ZkFprint.IsRegister)
+            {
+                ZkFprint.CancelEnroll();
+            }
+            ZkFprint.OnCapture += zkFprint_OnCapture;
+            ZkFprint.BeginCapture();
+            //ShowHintInfo("Please give fingerprint sample.");
+            //JObject json = JObject.Parse(temp);
+            /*string str = json11.template*/
+            JavaScriptSerializer j = new JavaScriptSerializer();
+            //System.Reflection.PropertyInfo pi = temp.GetType().GetProperty("template");
+            //String template = (String)(pi.GetValue(temp, null));
+            //String x = GetPropValue(json, "template");
+            char[] spearator = { '"', '"' };
+            Int32 count = 7;
+            String[] x = temp.Split(spearator, count, StringSplitOptions.RemoveEmptyEntries);
+            userTemplate = x[5];
+            //ShowHintInfo(userTemplate);
+        }
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+        public string webPostMethod(string postData, string URL)
+        {
+            string responseFromServer = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "POST";
+            request.Credentials = CredentialCache.DefaultCredentials;
+            ((HttpWebRequest)request).UserAgent =
+                              "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)";
+            request.Accept = "/";
+            request.UseDefaultCredentials = true;
+            request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            ShowHintInfo(responseFromServer);
+            return responseFromServer;
+        }
+        public string webGetMethod(string URL)
+        {
+            string jsonString = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.Credentials = CredentialCache.DefaultCredentials;
+            ((HttpWebRequest)request).UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)";
+            request.Accept = "/";
+            request.UseDefaultCredentials = true;
+            request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            WebResponse response = request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            jsonString = sr.ReadToEnd();
+            sr.Close();
+            //ShowHintInfo(jsonString);
+            return jsonString;
+        }
+
+
+
+        private void InitialAxZkfp()
+        {
+            try
+            {
+
+                ZkFprint.OnImageReceived += zkFprint_OnImageReceived;
+                //zkFprint.OnFingerTouching 
+                //zkFprint.OnFingerLeaving
+
+                if (ZkFprint.InitEngine() == 0)
+                {
+                    ZkFprint.FPEngineVersion = "9";
+                    ZkFprint.EnrollCount = 3;
+                    //deviceSerial.Text += " " + ZkFprint.SensorSN + " Count: " + ZkFprint.SensorCount.ToString() + " Index: " + ZkFprint.SensorIndex.ToString();
+                    //ShowHintInfo("Device successfully connected");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //ShowHintInfo("Device init err, error: " + ex.Message);
+            }
+        }
+
+        private void zkFprint_OnImageReceived(object sender, IZKFPEngXEvents_OnImageReceivedEvent e)
+        {
+            Graphics g = fpicture.CreateGraphics();
+            Bitmap bmp = new Bitmap(fpicture.Width, fpicture.Height);
+            g = Graphics.FromImage(bmp);
+            int dc = g.GetHdc().ToInt32();
+            ZkFprint.PrintImageAt(dc, 0, 0, bmp.Width, bmp.Height);
+            g.Dispose();
+            fpicture.Image = bmp;
+        }
+       
+        public void Play()
+        {
+            System.Threading.Timer t = new System.Threading.Timer(timerC, null, 30000, 30000);
+        }
+
+        private void timerC(object state)
+        {
+            //this.Close();
+            Environment.Exit(0);
+        }
+
+        private void zkFprint_OnCapture(object sender, IZKFPEngXEvents_OnCaptureEvent e)
+        {
+            string template = ZkFprint.EncodeTemplate1(e.aTemplate);
+
+                ShowHintInfo("Verified");
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://moproject.herokuapp.com/matching/");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+            if (ZkFprint.VerFingerFromStr(ref template, userTemplate, false, ref Check))
+            {
+               
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{\"matching\":\"Matched\"}";
+
+                    streamWriter.Write(json);
+                }
+
+                
+                this.Close();
+            }
+            else
+            {
+                ShowHintInfo("Not Verified");
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{\"matching\":\"Not Matched\"}";
+
+                    streamWriter.Write(json);
+                }
+            }
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+        }
+
+
+
+        private void ShowHintInfo(String s)
+        {
+            prompt.Text = s;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
